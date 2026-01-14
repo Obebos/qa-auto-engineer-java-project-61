f6c3eb9e2738b34162f206bd77719a9c2aa919f7
Палачев Максим <maxpalach1234@mail.ru>
2026-01-14 22:36:57 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("com.example:adependency:1.0")
+
 }
 
 tasks.register("checkstyle") {
